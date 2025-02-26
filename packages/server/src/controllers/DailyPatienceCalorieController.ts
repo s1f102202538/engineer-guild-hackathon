151d4fac48b1cef061d6eefc64be8b4dcd242790
s1f102202493 <kou052315@gmail.com>
2025-02-26 13:28:00 +0900

    Controllerに新しいエンドポイント作成　@Post('/convert-food-to-calories')

--- packages/server/src/controllers/DailyPatienceCalorieController.ts
+++ packages/server/src/controllers/DailyPatienceCalorieController.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 import { Controller, Res, Post, Body } from 'routing-controllers';
 import { injectable, inject } from 'inversify';
 import DailyPatienceCalorieService from '../services/DailyPatienceCalorieService';
+import OpenAI from '../services/OpenAI';
 
 import { TYPES } from '../config/types';
 import { UserClientIdRequest } from '../models/commonRequest';
@@ -12,13 +13,26 @@ class GetAllCalorieDataResponse {
   allCalorieData!: DailyPatienceCalorie[];
 }
 
+class ConvertFoodToCaloriesRequest {
+  food!: string;
+}
+
+class ConvertFoodToCaloriesResponse {
+  calories!: number;
+}
+
 @injectable()
 @Controller('/daily-patience-calorie')
 export default class DailyPatienceCalorieController {
   private dailyPatienceCalorieService: DailyPatienceCalorieService;
+  private openAI: OpenAI;
 
-  constructor(@inject(TYPES.DailyPatienceCalorieService) dailyPatienceCalorieService: DailyPatienceCalorieService) {
+  constructor(
+    @inject(TYPES.DailyPatienceCalorieService) dailyPatienceCalorieService: DailyPatienceCalorieService,
+    openAI: OpenAI
+  ) {
     this.dailyPatienceCalorieService = dailyPatienceCalorieService;
+    this.openAI = openAI;
   }
 
   @Post('/get-all-calorie-data')
@@ -40,6 +54,22 @@ export default class DailyPatienceCalorieController {
       return response.status(200).send({ allCalorieData });
     } catch (error) {
       console.error('DailyPatienceCalorieController:getAllCalorieData: ', error);
+      return response.status(500);
+    }
+  }
+
+  @Post('/convert-food-to-calories')
+  async convertFoodToCalories(
+    @Body() convertFoodToCaloriesRequest: ConvertFoodToCaloriesRequest,
+    @Res() response: Response<ConvertFoodToCaloriesResponse>
+  ) {
+    try {
+      const { food } = convertFoodToCaloriesRequest;
+      const calories = await this.openAI.ConvertFoodToCalories(food);
+
+      return response.status(200).send({ calories });
+    } catch (error) {
+      console.error('DailyPatienceCalorieController:convertFoodToCalories: ', error);
       return response.status(500);
     }
   }
