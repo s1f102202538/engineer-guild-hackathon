b720339dac9758e11f42e1d9039f723876d95a7a
NAGAO Manato <s1f102201829@iniad.org>
2025-02-28 04:25:05 +0900

    chat機能完成

--- packages/client/app/chat/_components/ChatMessages.tsx
+++ packages/client/app/chat/_components/ChatMessages.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { ChatLog } from 'app/services/ChatService';
 
 interface ChatMessagesProps {
@@ -6,9 +7,14 @@ interface ChatMessagesProps {
 
 const ChatMessages = (props: ChatMessagesProps) => {
   return (
-    <div className="flex-1 overflow-y-auto px-4 py-4">
+    <div className="flex-1 overflow-y-auto px-2 pl-0 py-4 pt-16">
       {props.chatLogs.map((chatLog, i) => (
-        <div key={i} className={`mb-4 ${!chatLog.isAI ? 'text-right' : ''}`}>
+        <div key={i} className={`mb-4 flex ${!chatLog.isAI ? 'justify-end' : ''}`}>
+          {chatLog.isAI && (
+            <div className="mr-2">
+              <Image src="/images/dog.png" alt="AI Icon" width={350} height={350} />
+            </div>
+          )}
           <div className={`inline-block p-3 rounded-xl ${!chatLog.isAI ? 'bg-[#4a665e] text-white' : 'bg-gray-100'}`}>
             {chatLog.message}
           </div>
