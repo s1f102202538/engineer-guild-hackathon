68cf8cfd8a0dbc75acf046fd297062fb88d782cf
s1f102202538 <s1f102202538@iniad.org>
2025-02-28 03:03:12 +0900

    コードの修正

--- packages/client/app/hooks/useClientId.ts
+++ packages/client/app/hooks/useClientId.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useAuth } from '@clerk/nextjs';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const useClientId = () => {
   const { userId } = useAuth();
