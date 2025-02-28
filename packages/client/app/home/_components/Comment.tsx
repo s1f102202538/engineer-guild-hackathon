5e57d868c5276c4a5808d28777b1d980d067f327
s1f102202538 <s1f102202538@iniad.org>
2025-02-28 14:50:38 +0900

    food uploadした時のサーバーとのつなぎこみ

--- packages/client/app/home/_components/Comment.tsx
+++ packages/client/app/home/_components/Comment.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
+import { useContext } from 'react';
+import { CommentContext } from '../page';
+import { UserDataContext } from '../page';
 
 const Comment = () => {
+  const comment = useContext(CommentContext);
+  const userData = useContext(UserDataContext);
+
+  const displayComment = comment !== '' ? comment : `${userData?.name}さん、おかえりなさい!`;
+
   return (
     <div className="relative w-full max-w-lg h-64 p-4">
       <div className="bg-green-100 rounded-xl p-3 ml-8 overflow-auto break-words">
-        <p className="font-semibold text-md">
-          texfasfaslfadslfjlsjfldsjf;lasdkfj;dlsdfsdfasfasfadfasdfasdfjfls;dkjf;ldjf;alsdjfl;asdjfladsjfads;lkfjldskjflksdjf;lksjfo;eijfn;;fjvo;dsfjl;sdjf;osdfj;osdjft
-        </p>
+        <p className="font-semibold text-md">{displayComment}</p>
       </div>
       {/* caractor */}
       <div className="absolute bottom-16 left-0 p-2 drop-shadow-xl">
-      <Image src="/images/dog.png" alt="dog" width={60} height={60} className="mr-2 cursor-pointer" />
+        <Image src="/images/dog.png" alt="dog" width={60} height={60} className="mr-2 cursor-pointer" />
       </div>
     </div>
   );
--- packages/client/app/home/page.tsx
+++ packages/client/app/home/page.tsx
@@ -16,12 +16,14 @@ import Header from '../components/Header';
 
 export const UserDataContext = createContext<UserData | null>(null);
 export const DailyPatienceCaloriesContext = createContext<number>(0);
+export const CommentContext = createContext<string>('');
 
 const Page = () => {
   const clientId = useClientId();
   const router = useRouter();
   const [userData, setUserData] = useState<UserData | null>(null);
   const [dailyPatienceCalories, setDailyPatienceCalories] = useState<number>(0);
+  const [comment, setComment] = useState<string>('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -46,20 +48,45 @@ const Page = () => {
     fetchData();
   }, [clientId, router]);
 
+  const fetchTodayCalorieData = async () => {
+    try {
+      const dailyPatienceCalorie = await DailyPatienceCalorieService.GetTodayCalorieData(clientId);
+      setDailyPatienceCalories(dailyPatienceCalorie.calories);
+    } catch (error) {
+      console.error('Failed to get today calorie date: ', error);
+    }
+  };
+
+  const uploadEndureFood = async (food: string) => {
+    const response = await DailyPatienceCalorieService.UploadFood(clientId, food);
+
+    setComment(response.message);
+  };
+
+  const onUplodaEndureFood = async (food: string) => {
+    await uploadEndureFood(food);
+
+    // TODO: 上手く取得できない可能性あり
+    // 本日の我慢カロリーを再取得
+    await fetchTodayCalorieData();
+  };
+
   return (
     <div>
       <UserDataContext.Provider value={userData}>
         <DailyPatienceCaloriesContext.Provider value={dailyPatienceCalories}>
-          <Header title="home" />
-          <div className="w-full mx-auto h-screen bg-beige-100 flex flex-col relative text-black">
-            <main className="flex-1 px-4 pt-4 pb-16 space-y-4 overflow-hidden">
-              <CheckWeight />
-              <EndureCalories />
-              <Comment />
-              <AddEndureCalories />
-            </main>
-            <Navbar />
-          </div>
+          <CommentContext.Provider value={comment}>
+            <Header title="home" />
+            <div className="w-full mx-auto h-screen bg-beige-100 flex flex-col relative text-black">
+              <main className="flex-1 px-4 pt-4 pb-16 space-y-4 overflow-hidden">
+                <CheckWeight />
+                <EndureCalories />
+                <Comment />
+                <AddEndureCalories onSubmit={onUplodaEndureFood} />
+              </main>
+              <Navbar />
+            </div>
+          </CommentContext.Provider>
         </DailyPatienceCaloriesContext.Provider>
       </UserDataContext.Provider>
     </div>
